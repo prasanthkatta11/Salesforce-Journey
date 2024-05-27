be53e7ec62cede6d78719c03cba6d52d1abc4aa9
Prasanth <165263804+prasanthkatta11@users.noreply.github.com>
2024-05-27 13:33:11 +1000

    screenQickActionsDemo

---
+++ force-app/main/default/lwc/screenQuickActionDemo/screenQuickActionDemo.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+import { LightningElement, api } from "lwc";
+import ACCOUNT_NAME from "@salesforce/schema/Account.Name";
+import ACCOUNT_INDUSTRY from "@salesforce/schema/Account.Industry";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { CloseActionScreenEvent } from "lightning/actions";
+
+export default class ScreenQuickActionDemo extends LightningElement {
+  @api recordId;
+  @api objectApiName;
+  fields = {
+    accountName: ACCOUNT_NAME,
+    accountIndustry: ACCOUNT_INDUSTRY
+  };
+
+  closeModal() {
+    this.dispatchEvent(new CloseActionScreenEvent());
+  }
+
+  successHandler() {
+    const event = new ShowToastEvent({
+      title: "Success",
+      message: "Record Saved Successfully.",
+      variant: "success"
+    });
+    this.dispatchEvent(event);
+    this.dispatchEvent(new CloseActionScreenEvent());
+  }
+}
