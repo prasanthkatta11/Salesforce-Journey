dbc3eb7a9311a2cbec7838370957444efbe3af6d
Prasanth <165263804+prasanthkatta11@users.noreply.github.com>
2024-06-01 00:15:13 +1000

    DataTables part 1

---
+++ force-app/main/default/lwc/customStyleDataTable/customStyleDataTable.js
@@ -0,0 +1,67 @@
+import { LightningElement, wire } from "lwc";
+import getcontactListDataTable from "@salesforce/apex/ContactListController.getcontactListDataTable";
+const columns = [
+  {
+    label: "Name",
+    type: "customName",
+    typeAttributes: {
+      contactName: { fieldName: "Name" }
+    }
+  },
+  {
+    label: "Account Name",
+    fieldName: "accountLink",
+    type: "url",
+    typeAttributes: {
+      label: { fieldName: "accountName" }
+    },
+    target: "_blank"
+  },
+  { label: "Title", fieldName: "Title" },
+  {
+    label: "Rank",
+    fieldName: "Rank__c",
+    type: "customRank",
+    typeAttributes: {
+      rankIcon: { fieldName: "rankIcon" }
+    }
+  },
+  { label: "Phone", fieldName: "Phone", type: "phone" },
+  { label: "Email", fieldName: "Email", type: "email" },
+  {
+    label: "Picture",
+    type: "customPicture",
+    typeAttributes: {
+      pictureUrl: { fieldName: "Picture__c" }
+    },
+    cellAttributes: {
+      Alignment: "centre"
+    }
+  }
+];
+
+export default class CustomStyleDataTable extends LightningElement {
+  contacts;
+  columns = columns;
+
+  @wire(getcontactListDataTable)
+  wiredcontacts({ data, error }) {
+    if (data) {
+      console.log("🚀 ~ CustomStyleDataTable ~ wiredcontacts ~ data:", data);
+      this.contacts = data.map((record) => {
+        let accountLink = "/" + record.AccountId;
+        let accountName = record.Account.Name;
+        let rankIcon = record.Rank__c > 1 ? "utility:ribbon" : "";
+        return {
+          ...record,
+          accountLink: accountLink,
+          accountName: accountName,
+          rankIcon: rankIcon
+        };
+      });
+      //this.contacts = data;
+    } else if (error) {
+      console.log("🚀 ~ CustomStyleDataTable ~ wiredcontacts ~ error:", error);
+    }
+  }
+}
